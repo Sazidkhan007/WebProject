d0d8fa6b71941924e53367d5612d368268b7d246
Sazid Khan <sazidmulla97@gmail.com>
2021-06-06 14:36:52 +0530

    add some feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+// somthing i did
 @NgModule({
   declarations: [
     AppComponent
