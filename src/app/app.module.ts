8ca76dd7f7a508dff0fdcd14d5abb6035ea7bf2d
Sazid Khan <sazidmulla97@gmail.com>
2021-06-06 22:56:51 +0530

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { UserListComponent } from './user-list/user-list.component';
 // somthing i did
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
